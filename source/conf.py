dfea606bdbd95ee755d1a177e7e931b819acd3f7
Navid M <navidmomtahen@gmail.com>
2026-04-01 01:59:12 +0100

    update theme

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 from sphinx.highlighting import lexers
